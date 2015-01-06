014a351c2ffc5b7308bb75f00e5a9522e4e67172
Jared Kerim <jared.l.kerim@gmail.com>
2015-01-06 23:26:57 +0000

    Rename d to data in validation.py

--- ichnaea/data/validation.py
+++ ichnaea/data/validation.py
@@ -66,58 +66,58 @@ def normalized_wifi_key(key):
     return key.lower()
 
 
-def normalized_dict_value(d, k, lo, hi, default=REQUIRED):
+def normalized_dict_value(data, k, lo, hi, default=REQUIRED):
     """
-    Returns a dict value d[k] if within range [lo,hi]. If the
+    Returns a dict value data[k] if within range [lo,hi]. If the
     Value is missing or out of range, return default, unless
     default is REQUIRED, in which case return None.
     """
-    if k not in d or d[k] < lo or d[k] > hi:
+    if k not in data or data[k] < lo or data[k] > hi:
         if default is REQUIRED:
             return None
         else:
             return default
     else:
-        return d[k]
+        return data[k]
 
 
-def normalized_dict(d, specs):
+def normalized_dict(data, specs):
     """
     Returns a copy of the provided dict, with its values set to a default
     value if missing or outside a specified range. If any missing or
     out-of-range values were specified as REQUIRED, return None.
 
     Arguments:
-    d -- a dict to normalize
+    data -- a dict to normalize
     specs -- a dict mapping keys to (lo, hi, default) triples, where
              default may be the symbolic constant REQUIRED;
              if any REQUIRED fields are missing or out of
              range, return None.
     """
-    if not isinstance(d, dict):
+    if not isinstance(data, dict):
         return None
 
     n = {}
     for (k, (lo, hi, default)) in specs.items():
-        v = normalized_dict_value(d, k, lo, hi, default)
+        v = normalized_dict_value(data, k, lo, hi, default)
         if v is None:
             return None
         n[k] = v
 
     # copy forward anything not specified
-    for (k, v) in d.items():
+    for (k, v) in data.items():
         if k not in n:
             n[k] = v
     return n
 
 
-def normalized_measure_dict(d):
+def normalized_measure_dict(data):
     """
-    Returns a normalized copy of the provided measurement dict d,
+    Returns a normalized copy of the provided measurement dict data,
     or None if the dict was invalid.
     """
-    d = normalized_dict(
-        d, dict(lat=(MIN_LAT, MAX_LAT, REQUIRED),
+    data = normalized_dict(
+        data, dict(lat=(MIN_LAT, MAX_LAT, REQUIRED),
                 lon=(-180.0, 180.0, REQUIRED),
                 heading=(0.0, MAX_HEADING, -1.0),
                 speed=(0, MAX_SPEED, -1.0),
@@ -125,24 +125,24 @@ def normalized_measure_dict(d):
                 altitude_accuracy=(0, MAX_ALTITUDE_ACCURACY, 0),
                 accuracy=(0, MAX_ACCURACY, 0)))
 
-    if d is None:
+    if data is None:
         return None
 
-    d['time'] = normalized_time(d.get('time', None))
+    data['time'] = normalized_time(data.get('time', None))
 
-    if 'report_id' not in d:
-        d['report_id'] = uuid.uuid1().hex
-    return d
+    if 'report_id' not in data:
+        data['report_id'] = uuid.uuid1().hex
+    return data
 
 
-def normalized_wifi_channel(d):
-    chan = int(d.get('channel', 0))
+def normalized_wifi_channel(data):
+    chan = int(data.get('channel', 0))
 
     if 0 < chan and chan < 166:
         return chan
 
     # if no explicit channel was given, calculate
-    freq = d.get('frequency', 0)
+    freq = data.get('frequency', 0)
 
     if 2411 < freq < 2473:
         # 2.4 GHz band
@@ -155,129 +155,129 @@ def normalized_wifi_channel(d):
     return 0
 
 
-def normalized_wifi_dict(d):
+def normalized_wifi_dict(data):
     """
-    Returns a normalized copy of the provided wifi dict d,
+    Returns a normalized copy of the provided wifi dict data,
     or None if the dict was invalid.
     """
-    d = normalized_dict(
-        d, dict(signal=(-200, -1, 0),
+    data = normalized_dict(
+        data, dict(signal=(-200, -1, 0),
                 signalToNoiseRatio=(0, 100, 0)))
 
-    if d is None:  # pragma: no cover
+    if data is None:  # pragma: no cover
         return None
 
-    if 'key' not in d:  # pragma: no cover
+    if 'key' not in data:  # pragma: no cover
         return None
 
-    d['key'] = normalized_wifi_key(d['key'])
+    data['key'] = normalized_wifi_key(data['key'])
 
-    if not valid_wifi_pattern(d['key']):
+    if not valid_wifi_pattern(data['key']):
         return None
 
-    d['channel'] = normalized_wifi_channel(d)
-    d.pop('frequency', 0)
+    data['channel'] = normalized_wifi_channel(data)
+    data.pop('frequency', 0)
 
-    if 'radio' in d:
-        del d['radio']
+    if 'radio' in data:
+        del data['radio']
 
-    return d
+    return data
 
 
-def normalized_wifi_measure_dict(d):
+def normalized_wifi_measure_dict(data):
     """
-    Returns a normalized copy of the provided wifi-measure dict d,
+    Returns a normalized copy of the provided wifi-measure dict data,
     or None if the dict was invalid.
     """
-    d = normalized_wifi_dict(d)
-    return normalized_measure_dict(d)
+    data = normalized_wifi_dict(data)
+    return normalized_measure_dict(data)
 
 
-def normalized_cell_dict(d, default_radio=-1):
+def normalized_cell_dict(data, default_radio=-1):
     """
-    Returns a normalized copy of the provided cell dict d,
+    Returns a normalized copy of the provided cell dict data,
     or None if the dict was invalid.
     """
-    if not isinstance(d, dict):  # pragma: no cover
+    if not isinstance(data, dict):  # pragma: no cover
         return None
 
-    d = d.copy()
-    if 'radio' in d and isinstance(d['radio'], basestring):
-        d['radio'] = RADIO_TYPE.get(d['radio'], -1)
+    data = data.copy()
+    if 'radio' in data and isinstance(data['radio'], basestring):
+        data['radio'] = RADIO_TYPE.get(data['radio'], -1)
 
-    d = normalized_dict(
-        d, dict(radio=(MIN_RADIO_TYPE, MAX_RADIO_TYPE, default_radio),
+    data = normalized_dict(
+        data, dict(radio=(MIN_RADIO_TYPE, MAX_RADIO_TYPE, default_radio),
                 mcc=(1, 999, REQUIRED),
                 mnc=(0, 32767, REQUIRED),
                 lac=(1, 65535, -1),
                 cid=(1, 268435455, -1),
                 psc=(0, 512, -1)))
 
-    if d is None:
+    if data is None:
         return None
 
     # Check against the list of all known valid mccs
-    if d['mcc'] not in ALL_VALID_MCCS:
+    if data['mcc'] not in ALL_VALID_MCCS:
         return None
 
     # If a default radio was set, and we don't know, use it as fallback
-    if d['radio'] == -1 and default_radio != -1:
-        d['radio'] = default_radio
+    if data['radio'] == -1 and default_radio != -1:
+        data['radio'] = default_radio
 
     # Skip CDMA towers missing lac or cid (no psc on CDMA exists to
     # backfill using inference)
-    if d['radio'] == RADIO_TYPE['cdma'] and (d['lac'] < 0 or d['cid'] < 0):
+    if data['radio'] == RADIO_TYPE['cdma'] and (data['lac'] < 0 or data['cid'] < 0):
         return None
 
     # Skip GSM/LTE/UMTS towers with an invalid MNC
-    if (d['radio'] in (
+    if (data['radio'] in (
             RADIO_TYPE['gsm'], RADIO_TYPE['umts'], RADIO_TYPE['lte'])
-            and d['mnc'] > 999):
+            and data['mnc'] > 999):
         return None
 
     # Treat cid=65535 without a valid lac as an unspecified value
-    if d['lac'] == -1 and d['cid'] == 65535:
-        d['cid'] = -1
+    if data['lac'] == -1 and data['cid'] == 65535:
+        data['cid'] = -1
 
     # Must have (lac and cid) or psc (psc-only to use in backfill)
-    if (d['lac'] == -1 or d['cid'] == -1) and d['psc'] == -1:
+    if (data['lac'] == -1 or data['cid'] == -1) and data['psc'] == -1:
         return None
 
     # If the cell id >= 65536 then it must be a umts tower
-    if d['cid'] >= 65536 and d['radio'] == RADIO_TYPE['gsm']:
-        d['radio'] = RADIO_TYPE['umts']
+    if data['cid'] >= 65536 and data['radio'] == RADIO_TYPE['gsm']:
+        data['radio'] = RADIO_TYPE['umts']
 
-    return d
+    return data
 
 
-def normalized_cell_measure_dict(d, measure_radio=-1):
+def normalized_cell_measure_dict(data, measure_radio=-1):
     """
-    Returns a normalized copy of the provided cell-measure dict d,
+    Returns a normalized copy of the provided cell-measure dict data,
     or None if the dict was invalid.
     """
-    d = normalized_cell_dict(d, default_radio=measure_radio)
-    d = normalized_measure_dict(d)
+    data = normalized_cell_dict(data, default_radio=measure_radio)
+    data = normalized_measure_dict(data)
 
     location_is_in_country = geocalc.location_is_in_country
-    if d is not None:
+    if data is not None:
         # Lat/lon must be inside one of the bounding boxes for the MCC.
-        lat = float(d['lat'])
-        lon = float(d['lon'])
+        lat = float(data['lat'])
+        lon = float(data['lon'])
         if not any([location_is_in_country(lat, lon, c.alpha2, 1)
-                    for c in mobile_codes.mcc(str(d['mcc']))]):
-            d = None
+                    for c in mobile_codes.mcc(str(data['mcc']))]):
+            data = None
 
-    if d is None:
+    if data is None:
         return None
 
-    if 'asu' in d and 'signal' in d:
-        # some clients send us a dBm value in the asu field, move it
+    if 'asu' in data and 'signal' in data:
+        # some clients sendata us a dBm value in the asu field, move it
         # over to the signal field before hitting validation
-        if d['asu'] < -1 and d['signal'] == 0:
-            d['signal'] = d['asu']
-            d['asu'] = -1
+        if data['asu'] < -1 and data['signal'] == 0:
+            data['signal'] = data['asu']
+            data['asu'] = -1
 
     return normalized_dict(
-        d, dict(asu=(0, 97, -1),
+        data, dict(asu=(0, 97, -1),
                 signal=(-150, -1, 0),
                 ta=(0, 63, 0)))
